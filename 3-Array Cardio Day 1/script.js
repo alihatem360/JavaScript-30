dcc168d65df680a24ab7e8c672eee886643432f2
Ali Hatem <68661572+alihatem360@users.noreply.github.com>
2022-07-23 15:25:23 +0200

    add 3-Array Cardio Day 1

---
+++ 3-Array Cardio Day 1/script.js
@@ -0,0 +1,165 @@
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+];
+
+const people = [
+  "Bernhard, Sandra",
+  "Bethea, Erin",
+  "Becker, Carl",
+  "Bentsen, Lloyd",
+  "Beckett, Samuel",
+  "Blake, William",
+  "Berger, Ric",
+  "Beddoes, Mick",
+  "Beethoven, Ludwig",
+  "Belloc, Hilaire",
+  "Begin, Menachem",
+  "Bellow, Saul",
+  "Benchley, Robert",
+  "Blair, Robert",
+  "Benenson, Peter",
+  "Benjamin, Walter",
+  "Berlin, Irving",
+  "Benn, Tony",
+  "Benson, Leana",
+  "Bent, Silas",
+  "Berle, Milton",
+  "Berry, Halle",
+  "Biko, Steve",
+  "Beck, Glenn",
+  "Bergman, Ingmar",
+  "Black, Elk",
+  "Berio, Luciano",
+  "Berne, Eric",
+  "Berra, Yogi",
+  "Berry, Wendell",
+  "Bevan, Aneurin",
+  "Ben-Gurion, David",
+  "Bevel, Ken",
+  "Biden, Joseph",
+  "Bennington, Chester",
+  "Bierce, Ambrose",
+  "Billings, Josh",
+  "Birrell, Augustine",
+  "Blair, Tony",
+  "Beecher, Henry",
+  "Biondo, Frank",
+];
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+const fifteneys = inventors.filter((inventor) => {
+  return inventor.year >= 1500 && inventor.year < 1599;
+});
+
+console.table(fifteneys);
+
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+const fullNames = inventors.map((inventor) => {
+  return `${inventor.first} ${inventor.last}`;
+});
+console.table(fullNames);
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+const sorted = inventors.sort((inventor1, inventor2) => {
+  if (inventor1.year > inventor2.year) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+const sorted2 = inventors.sort((inventor1, inventor2) => {
+  inventor1.year > inventor2.year ? 1 : -1;
+});
+console.table(sorted2);
+console.table(sorted);
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(totalYears); // 1589
+
+// 5. Sort the inventors by years lived
+
+const oldest = inventors.sort((inventor1, inventor2) => {
+  if (inventor1.passed - inventor1.year > inventor2.passed - inventor2.year) {
+    return -1;
+  } else {
+    return 1;
+  }
+});
+
+console.table(oldest);
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:
+
+// const catigory = document.querySelector(".mw-category");
+
+// // convert node List to array
+// const links = Array.from(catigory.querySelectorAll("a"));
+// const links2 = [...catigory.querySelectorAll("a")];
+
+// // catch inner  text from Links
+// const innerTexte = links2
+//   .map((link) => link.textContent)
+//   .filter((streetName) => streetName.includes("de"));
+// ==============================================================
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+
+const alphabetic = people.sort((lastOne, nextOne) => {
+  const [Alsat, Afirst] = lastOne.split(", ");
+  const [Blast, Bfirst] = nextOne.split(", ");
+  Alsat > Blast ? 1 : -1;
+});
+console.log(alphabetic);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+];
+
+const sumUpOfData = data.reduce((object, nextOne) => {
+  // if (!object[nextOne]) {
+  //   console.log(object[nextOne]);
+  //   object[nextOne] = 0;
+  // }
+  object[nextOne]++;
+  return object;
+}, {});
+
+console.log(sumUpOfData);
